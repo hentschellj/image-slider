fae366c78d1121e13f1de8373721e26af59ba94c
hentschellj <hentschellj@gmail.com>
2020-11-19 20:01:56 -0600

    Created sliding animation when nav buttons clicked

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,21 @@
 $(document).ready(function($) {
-  var imageBox      = $('.slider ul'),
-      imageWidth    = $('.slider ul li').first().children('img').width(),
-      imageQuantity = $('.slider ul').children('li').length;
+  const imageBox      = $('.slider ul'),
+        imageWidth    = $('.slider ul li').first().children('img').width(),
+        imageQuantity = $('.slider ul').children('li').length;
+  let currentImage    = 1;
 
   imageBox.css('width', imageWidth*imageQuantity);
 
   $('.nav button').on('click', function() {
     const whichButton = $(this).data('nav');
-    console.log(whichButton);
-  })
-})
+    if (whichButton === 'next') {
+      currentImage++;
+      const pxValue = -(currentImage - 1) * imageWidth;
+      imageBox.animate({'left' : pxValue});
+    } else if (whichButton === 'prev') {
+      currentImage--;
+      const pxValue = -(currentImage - 1) * imageWidth;
+      imageBox.animate({'left' : pxValue});
+    }
+  });
+});
