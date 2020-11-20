f4c5995b4cd9835879d44074799cb9abfc0a0abf
hentschellj <hentschellj@gmail.com>
2020-11-19 19:49:53 -0600

    Set dynamic width for slider list

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function($) {
+  var imageBox      = $('.slider ul'),
+      imageWidth    = $('.slider ul li').first().children('img').width(),
+      imageQuantity = $('.slider ul').children('li').length;
+
+  imageBox.css('width', imageWidth*imageQuantity);
+
   $('.nav button').on('click', function() {
     const whichButton = $(this).data('nav');
     console.log(whichButton);
